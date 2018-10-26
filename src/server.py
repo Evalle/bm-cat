bfd6d35aa513c4ecae5f5b3e14ea7c6745e83d5b
daxaker <itjustsoap@gmail.com>
2018-10-26 21:10:57 +0300

    rename refactoring

--- src/server.py
+++ src/server.py
@@ -1,6 +1,6 @@
 import hostbuilder
-from src.config import TOKEN, WEBHOOK_URL_BASE, WEBHOOK_URL_PATH, CERTIFICATE_PATH, WEBHOOK_LISTEN,\
-    PRIVATE_KEY_PATH, PORT, app, bot
+from src.config import TOKEN, WEBHOOK_URLBASE, WEBHOOK_URLPATH, CERTIFICATE_PATH, WEBHOOK_LISTEN,\
+    PRIVATEKEY_PATH, PORT, app, bot
 from telegram.ext import Updater
 
 # webserver entry point
@@ -11,11 +11,11 @@ if __name__ == "__main__":
     bot.deleteWebhook()
 
     # set new webhook
-    bot.setWebhook(WEBHOOK_URL_BASE+WEBHOOK_URL_PATH, certificate=open(CERTIFICATE_PATH, 'rb'))
+    bot.setWebhook(WEBHOOK_URLBASE+WEBHOOK_URLPATH, certificate=open(CERTIFICATE_PATH, 'rb'))
     # build and run server
     hostbuilder.create_host(app).run(host=WEBHOOK_LISTEN,
                                      port=PORT,
-                                     ssl_context=(CERTIFICATE_PATH, PRIVATE_KEY_PATH))
+                                     ssl_context=(CERTIFICATE_PATH, PRIVATEKEY_PATH))
 
 
 
