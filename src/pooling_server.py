0310bff316be6d333dcaf979d2d54443b8ea2f5c
daxaker <itjustsoap@gmail.com>
2018-10-29 23:37:02 +0300

    long pooling bot server script

---
+++ src/pooling_server.py
@@ -0,0 +1,18 @@
+from src.config import BMCAT_APIKEY
+from telegram.ext import Updater, MessageHandler, Filters
+
+
+# long pooling telegram bot for testing purposes
+updater = Updater(token=BMCAT_APIKEY)
+dispatcher = updater.dispatcher
+
+
+def echo(bot, update):
+    bot.send_message(chat_id=update.message.chat_id, text=update.message.text)
+
+
+echo_handler = MessageHandler(Filters.text, echo)
+dispatcher.add_handler(echo_handler)
+updater.start_polling()
+
+# updater.stop() don't forget stop pooling when your stop playing
