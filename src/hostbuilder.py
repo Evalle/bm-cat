112dde913c90eab97b086e49252e01fe13262e3f
daxaker <itjustsoap@gmail.com>
2018-10-29 23:08:39 +0300

    rename refactoring, comments

--- src/hostbuilder.py
+++ src/hostbuilder.py
@@ -4,18 +4,36 @@ from flask import request
 import telegram
 
 
-# server configuration point
 def create_host(app):
-    register_handler(app, webhook_handler)
+    """
+    configuration point of bm_cat server
+    you can do everything with app at this point to build host
+
+    :param app: flask server instance
+    :return: app
+    """
+    register_handler(app, bm_cat_handler)
     return app
 
 
 def register_handler(app, handler):
+    """
+    registration of handler for bm_cat bot server
+    only POST requests will processed
+
+    :param app: instance of flask server
+    :param handler: web request handler
+    :return: void
+    """
     app.add_url_rule('/' + BMCAT_APIKEY, 'handler', handler, methods=['POST'])
 
 
-# "echo" chatbot
-def webhook_handler():
+def bm_cat_handler():
+    """
+    bm_cat_handler() - root handler of bm_cat webhook
+
+    :return: OK - 200
+    """
     if request.method == "POST":
         # retrieve the message in JSON and then transform it to Telegram object
         update = telegram.Update.de_json(request.get_json(force=True), bot=bot)
