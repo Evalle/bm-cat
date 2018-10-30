5197c545200b19525ae44cad93c1cf3487364e0e
daxaker <itjustsoap@gmail.com>
2018-10-30 16:32:19 +0300

    webhook for inline queries

--- src/hostbuilder.py
+++ src/hostbuilder.py
@@ -2,6 +2,8 @@ from src.config import BMCAT_APIKEY, bot
 from flask import request
 
 import telegram
+from telegram import InlineQueryResultArticle, InputTextMessageContent
+
 from src.random_video_request_handler import response_random_video
 
 
@@ -37,8 +39,26 @@ def bm_cat_handler():
     """
     if request.method == "POST":
         update = telegram.Update.de_json(request.get_json(force=True), bot=bot)
+        inline_query = update.inline_query.query
         video_link = response_random_video(update)
+
         if video_link is not None:
-            bot.send_message(chat_id=update.message.chat.id, text=video_link)
+            send_message(inline_query is not None, update, video_link)
 
     return 'ok'
+
+
+def send_message(inline, update, video_link):
+    if inline:
+        results = list()
+        results.append(
+            InlineQueryResultArticle(
+                id=update.inline_query.query.upper(),
+                title='send link',
+                input_message_content=InputTextMessageContent(video_link)
+            )
+        )
+        # 10 seconds cache
+        bot.answer_inline_query(update.inline_query.id, results, cache_time=10)
+    else:
+        bot.send_message(chat_id=update.message.chat.id, text=video_link)
